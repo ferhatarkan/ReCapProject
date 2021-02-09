dd0d0b84a268df88577e015cdbac5cbcba423de4
ferhatarkan <55208799+ferhatarkan@users.noreply.github.com>
2021-02-09 13:05:12 +0300

    08.02.2021

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal:IEntityRepository<Color>,IColorDal
+    public class EfColorDal:IColorDal
     {
         public void Add(Color entity)
         {
